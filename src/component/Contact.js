a24c86930bf158554824a8825960471b6e4eb7e9
paulthechampion <enwerempaulo@gmail.com>
2022-04-03 15:04:56 -0400

    I think my mad scientist behavior has by passed this background wahala

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import star from "../img/star.jpeg"
 import Form from "./form"
+import moon from "../img/moon4.gif"
 import { useMediaQuery } from 'react-responsive'
+
 
 export default function Contact() {
   const isDesktopOrLaptop = useMediaQuery({
     query: '(min-width: 1085px)'
   })
-
+  if(isDesktopOrLaptop){
+    document.body.style.backgroundImage = "url('../img/moon2.gif')";
+  }
+  else {
+    document.body.style.backgroundImage = "url('../img/moon4.gif')";
+  }
   return (
     <div className='contact-div'>
       <div className='contact-flex-div'>
