b579fc4068f4a407202530b2a64964619985301b
paulthechampion <enwerempaulo@gmail.com>
2022-04-11 15:34:20 -0400

    think ive fixed the width problem

--- src/App.js
+++ src/App.js
@@ -36,4 +36,42 @@ function App() {
   );
 }
 
+
+window.addEventListener("load", function(){
+  const appearOptions = {
+      threshold:0,
+      rootMargin:"0px 0px -130px 0px"
+  }       
+
+  const faders = document.querySelectorAll(".fade-in")
+  const sliders = document.querySelectorAll(".slide-in")
+  const appearOnScroll = new IntersectionObserver
+  (function(
+      entries,
+      appearOnScroll
+  ){
+      entries.forEach(entry => {
+          if(!entry.isIntersecting){
+              return
+          }else{
+              entry.target.classList.add("appear")
+              appearOnScroll.unobserve(entry.target)
+          }
+  
+      })
+  },appearOptions)
+  
+  
+  faders.forEach(fader=>{
+      appearOnScroll.observe(fader)
+  })
+
+  sliders.forEach(slider=>{
+      appearOnScroll.observe(slider)
+  })
+
+});
+  
+
+
 export default App;
